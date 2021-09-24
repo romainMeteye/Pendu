6ff53a1db2ad1466b7a9a233e06c72ebc78a7fda
RomainMETEYE <romain.meteye.2017@gmail.com>
2021-09-24 10:11:14 +0200

    compete Pendu game's

--- js/pendu.js
+++ js/pendu.js
@@ -4,7 +4,7 @@ let maskWord = hiddenWord();
 let playerLife = 7;
 
 function randomWord() {
-    let wordList = ["lapin","voiture"]
+    let wordList = ["lapin","voiture","musique","boolean","shifoumi","wagonnet","bisbille","croquelardon","procrastination","panegyrique","volatille","fennec","aventure","robotique"]
    let number = Math.floor(Math.random() * wordList.length)
    return wordList[number];
 }
@@ -16,24 +16,44 @@ function hiddenWord() {
     return hidden;
 }
 function game() {
-    let enterLetter = prompt(`\n${playerLife} chances restantes, entrez une lettre pour jouer :\n ${maskWord.join(" ")}`);
-    if (enterLetter.length > 1 || enterLetter.length < 1) {
+    if ((maskWord.indexOf("_") === -1)) {
+        alert(`Bravo vous avez gagné ! Le mot était "${word}" !`);
+        confirm(`Rejouer ?`);
+        word = randomWord();
+        wordSplit = word.split("");
+        maskWord = hiddenWord();
+        playerLife = 7;
         game();
     }
-    else if (wordSplit.indexOf(enterLetter) === -1) {
-        playerLife--;
+    else if(playerLife === 0) {
+        alert(`Dommage, vous avez perdu... Le mot était "${word}" !`);
+        confirm(`Rejouer ?`);
+        word = randomWord();
+        wordSplit = word.split("");
+        maskWord = hiddenWord();
+        playerLife = 7;
         game();
     }
     else {
-        for(let n = 0; n < wordSplit.length ; n++) {
-            if (enterLetter === wordSplit[n]) {
+        let enterLetter = prompt(`\n${playerLife} chances restantes, entrez une lettre pour jouer :\n ${maskWord.join(" ")}`);
+        if (enterLetter.length > 1 || enterLetter.length < 1) {
+            game();
+        }
+        else if (wordSplit.indexOf(enterLetter) === -1) {
+            playerLife--;
+            game();
+        }
+        else {
+            for(let n = 0; n < wordSplit.length ; n++) {
+                if (enterLetter === wordSplit[n]) {
                 maskWord.splice(n, 1, enterLetter);
-            }
-            else {
-                 ""
                 }
+                else {
+                    ""
+                    }
+                }
+                return game();
             }
-            return game();
         }
 }
 alert("Bonjour et bienvenue dans le jeu du Pendu !");
